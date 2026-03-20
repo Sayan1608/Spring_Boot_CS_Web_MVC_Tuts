f7bdf9967f70b1278d3b4e23f1ca73563e0dc9e7
sayan <subhosen16@gmail.com>
2026-03-20 12:42:22 +0530

    Setting Request Param

--- src/main/java/com/cs/springbootwebtuts/controllers/EmployeeController.java
+++ src/main/java/com/cs/springbootwebtuts/controllers/EmployeeController.java
@@ -1,10 +1,7 @@
 package com.cs.springbootwebtuts.controllers;
 
 import com.cs.springbootwebtuts.dto.EmployeeDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/employees")
@@ -20,6 +17,11 @@ public class EmployeeController {
         return new
                 EmployeeDto(id, "John Doe", "johndoe@gmail.com", 30,
                 null, true);
+    }
+
+    @GetMapping
+    public String getRequestParamTest(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+        return "Name: " + name + ", Age: " + age;
     }
 
 
