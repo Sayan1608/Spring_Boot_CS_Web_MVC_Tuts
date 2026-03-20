6fbd75b17394379172ba1c6a0bd4af637da007ee
sayan <subhosen16@gmail.com>
2026-03-20 12:39:14 +0530

    Setting my get Mapping to fetch employee-- Path Variable

--- src/main/java/com/cs/springbootwebtuts/controllers/EmployeeController.java
+++ src/main/java/com/cs/springbootwebtuts/controllers/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.cs.springbootwebtuts.controllers;
 
+import com.cs.springbootwebtuts.dto.EmployeeDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,13 @@ public class EmployeeController {
     public String testEmployeeController(){
         return "Hello Employee Controller";
     }
+
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDto getEmployee(@PathVariable(name = "employeeId") Long id){
+        return new
+                EmployeeDto(id, "John Doe", "johndoe@gmail.com", 30,
+                null, true);
+    }
+
+
 }
