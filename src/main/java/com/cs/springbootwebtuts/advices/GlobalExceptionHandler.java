93bd8535bb6f7fe1ee2ff51a5958fd250b125bb7
sayan <subhosen16@gmail.com>
2026-03-22 19:35:32 +0530

    Code Review Fixes

--- src/main/java/com/cs/springbootwebtuts/advices/GlobalExceptionHandler.java
+++ src/main/java/com/cs/springbootwebtuts/advices/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.cs.springbootwebtuts.advices;
 
 import com.cs.springbootwebtuts.exceptions.ResourceNotFoundException;
-import org.jspecify.annotations.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
